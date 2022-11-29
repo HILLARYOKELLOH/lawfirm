c4264626c94487cf89d5c0472a5d07d12dcfad6d
HILLARY OPANY <hillaryokello19@gmail.com>
2022-11-29 17:06:29 +0300

    first commit

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Kanun - Law Firm Website Template</title>
+    <title>Kanun Law Firm</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="Law Firm Website Template" name="keywords">
     <meta content="Law Firm Website Template" name="description">
